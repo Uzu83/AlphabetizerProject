bdf4cea8fe913c80cf4cf4c3c22dc44eb6e96d8e
Toshiki Togo <108942850+Uzu83@users.noreply.github.com>
2024-11-09 16:10:25 +0900

    Initial Commit

---
+++ Alphabetizer/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Alphabetizer
+//
+//  Created by Uzu83 on 2024/11/09
+//
+//
+
+import Foundation
+
+enum Message: String{
+    case instructions = "Place the tiles in alphabetical order"
+    case tryAgain = "Almost 😅"
+    case youWin = "You win! 🏆"
+}
